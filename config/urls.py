c7da654af24e17a9488505fa151825b98ec2ba14
Artur <arturunkulov30@gmail.com>
2024-12-28 00:27:19 +0600

    udpate code statufdddd

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,9 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 # Настройка для drf-yasg (Swagger)
 schema_view = get_schema_view(
     openapi.Info(
@@ -29,3 +32,5 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),  # Для экспорта схемы
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
