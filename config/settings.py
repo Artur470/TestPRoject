56c84679268ad2a72985f5b6ac94e05e8efde611
Artur <arturunkulov30@gmail.com>
2024-12-28 19:01:29 +0600

    ugpalmrgqragreg

--- config/settings.py
+++ config/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'drf_yasg',
-    'django_extensions',
+
     'core',
     'rest_framework',
     'rest_framework.authtoken',
